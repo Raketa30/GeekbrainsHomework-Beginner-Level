0885d7047e9e953163f9ac4ea3100a61e2322a56
Igor Kulgachov <kulgachov88@gmail.com>
2021-01-29 21:15:42 +0300

    Created and Initialized primitive and reference variables

--- src/ru/geekbrains/lesson1/Intro.java
+++ src/ru/geekbrains/lesson1/Intro.java
@@ -2,6 +2,21 @@ package ru.geekbrains.lesson1;
 //  1. Создать пустой проект в IntelliJ IDEA и прописать метод main().
 public class Intro {
     public static void main(String[] args) {
+//      2. Создать переменные всех пройденных типов данных и инициализировать их значения.
 
+        byte b = 127;
+        short s = 32766;
+
+        char ch = 'a';
+        char ch2 = 97;
+        char ch3 = '\u0061';
+
+        int i = 3253443;
+        long l = 1000_000_000_000_000L;
+        float f = 10.10f;
+        double d = 22.234;
+        boolean bln = true;
+
+        String str = "Hello geekbrains!";
     }
 }
