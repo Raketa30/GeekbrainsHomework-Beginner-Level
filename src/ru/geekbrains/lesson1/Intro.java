f086b15a54eaf6c054bee6f28f58c6cfa149e253
Igor Kulgachov <kulgachov88@gmail.com>
2021-01-29 21:18:29 +0300

    Stage 3 Done! Created expression a * (b + (c / d)) Calculating method

--- src/ru/geekbrains/lesson1/Intro.java
+++ src/ru/geekbrains/lesson1/Intro.java
@@ -19,4 +19,11 @@ public class Intro {
 
         String str = "Hello geekbrains!";
     }
+    
+//    3. Написать метод, вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
+//    где a, b, c, d – аргументы этого метода, имеющие тип float.
+
+    private static float expressionCalculating(float a, float b, float c, float d) {
+        return a * (b + (c / d));
+    }
 }
