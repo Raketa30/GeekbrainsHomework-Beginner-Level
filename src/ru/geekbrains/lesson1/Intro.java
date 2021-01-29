ec289ba9c7fec3dad16a3d043b9e62c64757e408
Igor Kulgachov <kulgachov88@gmail.com>
2021-01-29 20:55:34 +0300

    Created new class and main method

---
+++ src/ru/geekbrains/lesson1/Intro.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.lesson1;
+//  1. Создать пустой проект в IntelliJ IDEA и прописать метод main().
+public class Intro {
+    public static void main(String[] args) {
+
+    }
+}
