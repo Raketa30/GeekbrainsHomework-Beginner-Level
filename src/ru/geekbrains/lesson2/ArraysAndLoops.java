7fac94559dec9c6ce916eb21d56da044dc9a219a
duckpool <Grem32167>
2021-01-31 16:10:27 +0300

    2 Task Done!

--- src/ru/geekbrains/lesson2/ArraysAndLoops.java
+++ src/ru/geekbrains/lesson2/ArraysAndLoops.java
@@ -8,7 +8,14 @@ public class ArraysAndLoops {
 //        Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
 //        С помощью цикла и условия заменить 0 на 1, 1 на 0;
         int[] firstArray = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println("Task 1");
         System.out.println(Arrays.toString(change0to1(firstArray)));
+
+//        2. Задать пустой целочисленный массив размером 8.
+//        С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+        int size = 8;
+        System.out.println("Task 2");
+        System.out.println(Arrays.toString(fillArray(size)));
     }
 
     private static int[] change0to1(int[] arr){
@@ -22,5 +29,15 @@ public class ArraysAndLoops {
         }
 
         return changed;
+    }
+
+    private static int[] fillArray(int size) {
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = i * 3;
+        }
+
+        return arr;
     }
 }
