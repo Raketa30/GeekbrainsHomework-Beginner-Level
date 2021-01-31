48003462b46420e0fd09ec27ca8dff8150ef8bcf
duckpool <Grem32167>
2021-01-31 16:22:27 +0300

    3 Task Done!

--- src/ru/geekbrains/lesson2/ArraysAndLoops.java
+++ src/ru/geekbrains/lesson2/ArraysAndLoops.java
@@ -9,6 +9,7 @@ public class ArraysAndLoops {
 //        С помощью цикла и условия заменить 0 на 1, 1 на 0;
         int[] firstArray = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         System.out.println("Task 1");
+        System.out.println(Arrays.toString(firstArray));
         System.out.println(Arrays.toString(change0to1(firstArray)));
 
 //        2. Задать пустой целочисленный массив размером 8.
@@ -16,6 +17,13 @@ public class ArraysAndLoops {
         int size = 8;
         System.out.println("Task 2");
         System.out.println(Arrays.toString(fillArray(size)));
+
+//        3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ]
+//        пройти по нему циклом, и числа меньшие 6 умножить на 2;
+        int[] thirdArray = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println("Task 3");
+        System.out.println(Arrays.toString(findDigitsLowerThanSix(thirdArray)));
+
     }
 
     private static int[] change0to1(int[] arr){
@@ -39,5 +47,19 @@ public class ArraysAndLoops {
         }
 
         return arr;
+    }
+
+    private static int[] findDigitsLowerThanSix(int[] arr) {
+        int[] changedArr = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++)  {
+            if (arr[i] < 6) {
+                changedArr[i] = arr[i] * 2;
+            } else {
+                changedArr[i] = arr[i];
+            }
+        }
+
+        return changedArr;
     }
 }
